76efe663e7c9319a7de7a6dc3f952e213e66482b
Julien Bischoff <julien.bischoff@viacesi.fr>
2020-12-14 11:20:21 +0100

    Add Jump

--- Assets/vSchool/Scripts/PlayerMovement.cs
+++ Assets/vSchool/Scripts/PlayerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float Speed = 5.0f;
     public float Gravity = -14.0f;
+    public float JumpForce = 5f;
+
     private CharacterController characterController;
     private Vector3 movement;
+    private float verticalForce;
 	// Use this for initialization
 	void Start () {
         characterController = GetComponent<CharacterController>();
@@ -15,8 +18,23 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        movement = new Vector3(Input.GetAxis("Horizontal") * Speed, 0, Input.GetAxis("Vertical") * Speed);
+        movement = new Vector3(Input.GetAxis("Horizontal") * Speed, verticalForce, Input.GetAxis("Vertical") * Speed);
         movement = transform.TransformDirection(movement);
         characterController.Move(movement * Time.deltaTime);
-	}
+
+        if (characterController.isGrounded)
+        {
+            verticalForce = 0;
+        }
+        else
+        {
+            verticalForce += Gravity * Time.deltaTime;
+        }
+
+        if (Input.GetButton("Jump") && characterController.isGrounded)
+        {
+            verticalForce = JumpForce;
+        }
+        
+    }
 }
