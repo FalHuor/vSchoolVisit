150b47da26d854daa1eac8a6a0f5e5477f033b36
CorentinMlo <corentin.millio@viacesi.fr>
2020-12-14 12:48:09 +0100

    Lightning

---
+++ Assets/vSchool/Scripts/SwitchBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBehaviour : MonoBehaviour {
+
+	private LightsBehaviour LightsBehaviour;
+
+	// Use this for initialization
+	void Start()
+	{
+		LightsBehaviour = GameObject.Find("Ceiling_lights").GetComponent<LightsBehaviour>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.CompareTag("Player"))
+		{
+
+		}
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		if (collision.collider.CompareTag("Player"))
+		{
+			/*LightsBehaviour.isActive = false;
+			Debug.Log(LightsBehaviour.isActive);*/
+		}
+	}
+}
