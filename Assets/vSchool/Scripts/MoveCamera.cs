e527cceeb367ec362e62b36e55035d07e000ca9e
BenjaminDardy <benjamin.dardy@viacesi.fr>
2020-12-14 11:59:52 +0100

    Add sound

---
+++ Assets/vSchool/Scripts/MoveCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+    public enum RotationAxis
+    {
+        X = 1,
+        Y = 2
+    }
+    public RotationAxis Axes = RotationAxis.X;
+    public float SensHorizontal = 5.0f;
+    public float SensVertical = 5.0f;
+    public float MinimumVerticalAngle = -75.0f;
+    public float MaximumVerticalAngle = 55.0f;
+
+    private float rotationX = 0;
+    private float rotationY = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Axes == RotationAxis.X)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * SensHorizontal, 0);
+        }
+        else if (Axes == RotationAxis.Y)
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * SensVertical;
+            rotationX = Mathf.Clamp(rotationX, MinimumVerticalAngle, MaximumVerticalAngle);
+            rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+    }
+}
