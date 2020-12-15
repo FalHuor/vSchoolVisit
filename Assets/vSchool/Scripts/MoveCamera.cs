ff1fb864058be0ff08d612fd39ac149428dfc4fc
CorentinMlo <corentin.millio@viacesi.fr>
2020-12-15 16:33:41 +0100

    AddCrosshair

--- Assets/vSchool/Scripts/MoveCamera.cs
+++ Assets/vSchool/Scripts/MoveCamera.cs
@@ -19,8 +19,9 @@ public class MoveCamera : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
